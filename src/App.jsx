39a2c9307679ba8304ca0e5bb79253679b3278be
Liam Stienen <liam.stienen@gmail.com>
2026-04-13 19:22:51 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "sonner"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { pagesConfig } from './pages.config'
@@ -72,6 +73,7 @@ function App() {
           <AuthenticatedApp />
         </Router>
         <Toaster />
+        <SonnerToaster position="top-right" />
       </QueryClientProvider>
     </AuthProvider>
   )
